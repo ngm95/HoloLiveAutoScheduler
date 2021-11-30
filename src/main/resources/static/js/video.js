c57ba7abe85de006fe98205b583234d65090bc54
ngm95 <51411433+ngm95@users.noreply.github.com>
2021-11-30 20:26:14 +0900

    21.11.30 작업
    1. 24시간 호스팅 성공적
    2. 멀티 뷰 페이지 오류 수정

--- src/main/resources/static/js/video.js
+++ src/main/resources/static/js/video.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	var token = $("meta[name='_csrf_token']").attr("content");
 	var header = $("meta[name='_csrf_header']").attr("content");
 	
-	$('#iframeDiv').height((window.innerHeight - 80));
+	$('#iframeDiv').height((window.innerHeight - 70));
 	$('#iframeDiv').width((window.innerWidth));
 	
 	var sizeArr = [[[24, 24]], [[12, 24], [12, 24]], [[12, 24], [12, 12], [12, 12]], [[12, 12], [12, 12], [12, 12], [12, 12]], [[12, 15], [12, 15], [8, 9], [8, 9], [8, 9]], [[8, 12], [8, 12], [8, 12], [8, 12], [8, 12], [8, 12]],
@@ -14,8 +14,10 @@ $(document).ready(function() {
 	var videos = 0;
 	
 
-	var subWidth = 2;
+	var subWidth = 0;
 	var subHeight = 28;
+	
+	var selectedProfile = new Array();
 	
 //	function setResizeSensor(resizeDiv) {
 //		new ResizeSensor(resizeDiv, function(size) {
@@ -25,6 +27,8 @@ $(document).ready(function() {
 //			resizeDiv.attr('id', 'resized');
 //		});
 //	}
+
+	refresh();
 
 	function refresh() {
 		$('.tooltip').each(function() {
@@ -54,9 +58,9 @@ $(document).ready(function() {
 					if (unique == true) {
 						var html = '<div class=\'d-flex flex-column\'><div class=\'d-flex flex-row\'><img src=\'' + videos[i].thumbnailPath + '\' width=\'100%\' height=\'100%\'></div><div class=\'d-flex flex-row\'>예정 시각 : ' + videos[i].scheduledStartTime + '</div></div>';
 						if (videos[i].actualStartTime != null) {
-							$('#videosList').append("<div name=\"navProfile\" id=\"" + videos[i].videoId + "\" class=\"d-flex flex-column\"><button name=\"tooltip\" id=\"" + videos[i].videoId + "\" type=\"button\" class=\"btn btn-style\" style=\"padding:0px 0px 0px 0px\" data-bs-toggle=\"tooltip\" data-bs-html=\"true\" title=\"" + html + "\"><img src=\"" + videos[i].profilePath + "\" style=\"border-radius:50%; width:65px; border:2px red solid\"></button></div>");
+							$('#videosList').append("<div name=\"tooltip\" id=\"" + videos[i].videoId + "\" data-bs-toggle=\"tooltip\" data-bs-html=\"true\" title=\"" + html + "\" style=\"order:"+i+"; padding-left:2px; padding-right:2px\"><img src=\"" + videos[i].profilePath + "\" style=\"border-radius:50%; width:55px; border:2px red solid\"></div>");
 						} else {
-							$('#videosList').append("<div name=\"navProfile\" id=\"" + videos[i].videoId + "\" class=\"d-flex flex-column\"><button name=\"tooltip\" id=\"" + videos[i].videoId + "\" type=\"button\" class=\"btn btn-style\" style=\"padding:0px 0px 0px 0px\" data-bs-toggle=\"tooltip\" data-bs-html=\"true\" title=\"" + html + "\"><img src=\"" + videos[i].profilePath + "\" style=\"border-radius:50%; width:65px; border:2px green solid\"></button></div>");
+							$('#videosList').append("<div name=\"tooltip\" id=\"" + videos[i].videoId + "\" data-bs-toggle=\"tooltip\" data-bs-html=\"true\" title=\"" + html + "\" style=\"order:"+i+"; padding-left:2px; padding-right:2px\"><img src=\"" + videos[i].profilePath + "\" style=\"border-radius:50%; width:55px; border:2px green solid\"></div>");
 						}
 					}
 				}
@@ -84,8 +88,8 @@ $(document).ready(function() {
 				sizeInfo = sizeArr[videos-1][index];
 				var divWidth = width*sizeInfo[0]-subWidth;
 				var divHeight = height*sizeInfo[1]-subHeight;
-				console.log($('#iframeDiv').width() + ", " + $('#iframeDiv').height() + " + " + sizeInfo[0] + ", " + sizeInfo[1] + " -> " + divWidth + ', ' + divHeight);
-				$(this).find('iframe').width(divWidth);
+				
+				$(this).find('iframe').width(divWidth-2);
 				$(this).find('iframe').height(divHeight);
 				
 				posInfo = posArr[videos-1][index];
@@ -97,8 +101,8 @@ $(document).ready(function() {
 				sizeInfo = sizeArr[videos-1][index];
 				var divWidth = width*sizeInfo[1]-subWidth;
 				var divHeight = height*sizeInfo[0]-subHeight;
-				console.log($('#iframeDiv').width() + ", " + $('#iframeDiv').height() + " + " + sizeInfo[0] + ", " + sizeInfo[1] + " -> " + divWidth + ', ' + divHeight);
-				$(this).find('iframe').width(divWidth);
+				
+				$(this).find('iframe').width(divWidth-2);
 				$(this).find('iframe').height(divHeight);
 				
 				posInfo = posArr[videos-1][index];
@@ -112,7 +116,7 @@ $(document).ready(function() {
 	}
 	
 	$(window).resize(function() {
-		$('#iframeDiv').height((window.innerHeight - 80));
+		$('#iframeDiv').height((window.innerHeight - 70));
 		$('#iframeDiv').width((window.innerWidth))
 		
 		resize();
@@ -121,34 +125,7 @@ $(document).ready(function() {
 	$('#refresh').click(function() {
 		refresh();
 	});
-
-	$.ajax({
-		url: "/livestream/videoInfo",
-		method: "get",
-		dataType: "json",
-		contentType: "application/json;charset=UTF-8",
-		beforeSend: function(xhr) {
-			xhr.setRequestHeader(header, token);
-		},
-		success: function(videos) {
-			for (var i in videos) {
-				var html = '<div class=\'d-flex flex-column\'><div class=\'d-flex flex-row\'><img src=\'' + videos[i].thumbnailPath + '\' width=\'100%\' height=\'100%\'></div><div class=\'d-flex flex-row\'>예정 시각 : ' + videos[i].scheduledStartTime + '</div></div>';
-				if (videos[i].actualStartTime != null) {
-					$('#videosList').append("<div name=\"navProfile\" id=\"" + videos[i].videoId + "\" class=\"d-flex flex-column\"><button name=\"tooltip\" id=\"" + videos[i].videoId + "\" type=\"button\" class=\"btn btn-style\" style=\"padding:0px 0px 0px 0px\" data-bs-toggle=\"tooltip\" data-bs-html=\"true\" title=\"" + html + "\"><img src=\"" + videos[i].profilePath + "\" style=\"border-radius:50%; width:65px; border:2px red solid\"></button></div>");
-				} else {
-					$('#videosList').append("<div name=\"navProfile\" id=\"" + videos[i].videoId + "\" class=\"d-flex flex-column\"><button name=\"tooltip\" id=\"" + videos[i].videoId + "\" type=\"button\" class=\"btn btn-style\" style=\"padding:0px 0px 0px 0px\" data-bs-toggle=\"tooltip\" data-bs-html=\"true\" title=\"" + html + "\"><img src=\"" + videos[i].profilePath + "\" style=\"border-radius:50%; width:65px; border:2px green solid\"></button></div>");
-				}
-			}
-			$('[name="tooltip"]').each(function() {
-				var tooltip = new bootstrap.Tooltip($(this), {
-					boundary: document.body // or document.querySelector('#boundary')
-				});
-			});
-		},
-		error: function() {
-			$('#videosList').append("<p>오류가 발생했습니다.</p>");
-		}
-	});
+	
 	
 	var myModal = new bootstrap.Modal(document.getElementById('noticeModal'), {
 		keyboard: false
@@ -175,25 +152,34 @@ $(document).ready(function() {
 				return;
 			}
 			
-			$('#iframeDiv').append("<div id=\"video\"><div class=\"d-flex flex-column\" style=\"border:1px gray solid; border-radius:5px;\"><div clas=\"d-flex\"><iframe id=\"" + videoId + "\" width=\"" + $('#iframeDiv').width()/24 + "\" height=\"" + $('#iframeDiv').height()/24 + "\" src=\"https://www.youtube.com/embed/" + videoId + "\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe></div><div class=\"d-flex justify-content-end\"><div class=\"d-flex\" style=\"font-size:1.0em\">↑ 영상 제거 : &nbsp;</div><div class=\"d-flex\"><button class=\"btn btn-sm btn-outline-secondary\" id=\"detach\" type=\"button\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-x-circle-fill\" viewBox=\"0 0 16 16\"><path d=\"M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z\"/></svg></button></div><div></div></div>");
+			selectedProfile.push($(this));
+			$('#iframeDiv').append("<div id=\"video\"><div class=\"d-flex flex-column\" style=\"border:1px gray solid; border-radius:5px;\"><div clas=\"d-flex\"><iframe id=\"" + videoId + "\" width=\"" + $('#iframeDiv').width()/24 + "\" height=\"" + $('#iframeDiv').height()/24 + "\" src=\"https://www.youtube.com/embed/" + videoId + "\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe></div><div class=\"d-flex justify-content-end\"><div class=\"d-flex\" style=\"font-size:1.0em\">↑ 영상 제거 : &nbsp;</div><div class=\"d-flex\"><button class=\"btn btn-sm btn-outline-secondary\" id=\""+ videoId +"\" name=\"detach\" type=\"button\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-x-circle-fill\" viewBox=\"0 0 16 16\"><path d=\"M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z\"/></svg></button></div><div></div></div>");
+
 //			var added = $('#iframeDiv').children(':last');
 //			setResizeSensor(added);
-
+			$(this).remove();
+			$('.tooltip').remove();
 			videos += 1;
 			resize();
-			refresh();
 		} else {
 			showNoticeModal();
 		}
 	});
 
-	$(document).on('click', '#detach', function() {
+	$(document).on('click', '[name="detach"]', function() {
+		var videoId = $(this).attr('id');
 		$(this).parent().closest('#video').remove();
-		
 		videos -= 1;
 		
+		for (var i = 0; i < selectedProfile.length; i++) {
+			console.log(selectedProfile[i].attr('id') + ", " + videoId);
+			if (selectedProfile[i].attr('id') == videoId) {
+				$('#videosList').append(selectedProfile[i]);
+				selectedProfile.splice(i, 1);
+			}
+		}
+		
 		resize();
-		refresh();
 	});
 	
 	$(document).on('click', '[name="select"]', function() {
@@ -202,12 +188,15 @@ $(document).ready(function() {
 		}
 		else {
 			var videoId = $(this).attr('id');
-			$('#iframeDiv').append("<div id=\"video\"><div class=\"d-flex flex-column\" style=\"border:1px gray solid; border-radius:5px;\"><div clas=\"d-flex\"><iframe id=\"" + videoId + "\" width=\"" + $('#iframeDiv').width()/24 + "\" height=\"" + $('#iframeDiv').height()/24 + "\" src=\"https://www.youtube.com/embed/" + videoId + "\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe></div><div class=\"d-flex justify-content-end\"><div class=\"d-flex\" style=\"font-size:1.0em\">↑ 영상 제거 : &nbsp;</div><div class=\"d-flex\"><button class=\"btn btn-sm btn-outline-secondary\" id=\"detach\" type=\"button\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-x-circle-fill\" viewBox=\"0 0 16 16\"><path d=\"M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z\"/></svg></button></div><div></div></div>");
+			$('#iframeDiv').append("<div id=\"video\"><div class=\"d-flex flex-column\" style=\"border:1px gray solid; border-radius:5px;\"><div clas=\"d-flex\"><iframe id=\"" + videoId + "\" width=\"" + $('#iframeDiv').width()/24 + "\" height=\"" + $('#iframeDiv').height()/24 + "\" src=\"https://www.youtube.com/embed/" + videoId + "\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe></div><div class=\"d-flex justify-content-end\"><div class=\"d-flex\" style=\"font-size:1.0em\">↑ 영상 제거 : &nbsp;</div><div class=\"d-flex\"><button class=\"btn btn-sm btn-outline-secondary\" id=\""+ videoId +"\" name=\"detach\" type=\"button\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-x-circle-fill\" viewBox=\"0 0 16 16\"><path d=\"M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z\"/></svg></button></div><div></div></div>");
 			
 			videos += 1;
 			resize();
 		}
-		
-		refresh();
+	});
+	
+	$(document).on('click', '[name="reload"]', function() {
+		var src = $(this).parent(':first').find('iframe').attr('src');
+		$(this).parent(':first').find('iframe').attr('src', src);
 	});
 });
