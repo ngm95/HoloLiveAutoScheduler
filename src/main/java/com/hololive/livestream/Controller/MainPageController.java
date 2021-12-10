8354b2df3af3355aaeb73d426a746d61726df3d6
ngm95 <51411433+ngm95@users.noreply.github.com>
2021-12-10 14:13:15 +0900

    21.12.10 Collect메소드 주석처리

--- src/main/java/com/hololive/livestream/Controller/MainPageController.java
+++ src/main/java/com/hololive/livestream/Controller/MainPageController.java
@@ -30,76 +30,76 @@ public class MainPageController {
 		return "/index";
 	}
 	
-	@RequestMapping("/collect")
-	public String collect() {
-		checkChannel();
-		return "/collect";
-	}
-
-	@Autowired
-	private VideoService videoServ;
-
-	ObjectMapper objectMapper = new ObjectMapper();
-	DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
-
-	@Async("executor")
-	public void checkChannel() {
-		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		List<MemberDTO> members = videoServ.readAllMember();
-
-		for (MemberDTO member : members) {
-			System.out.println(member.getMemberName());
-			int offset = 0;
-			try {
-				while (true) {
-					long startTime = Calendar.getInstance().getTimeInMillis();
-					System.out.print("\toffset : " + offset + " => ");
-					
-					String videoURL = "https://holodex.net/api/v2/videos?channel_id=" + member.getChannelId() + "&status=past&type=stream&include=live_info&order=asc&limit=50&offset=" + offset;
-					
-					HttpRequest request = HttpRequest.newBuilder().uri(URI.create(videoURL))
-							.header("Content-Type", "application/json").header("X-APIKEY", "27e71dd9-f108-4896-875e-5a91207a63fe")
-							.method("GET", HttpRequest.BodyPublishers.noBody()).build();
-					HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-					List<VideoAPI> videosFromAPI = objectMapper.readValue(response.body(), objectMapper.getTypeFactory().constructCollectionType(List.class, VideoAPI.class));
-					if (videosFromAPI.size() == 0) {
-						System.out.println("더 이상 영상이 없습니다.");
-						break;
-					}
-					else {
-						int recorded = 0;
-						for (int i = 0; i < videosFromAPI.size(); i++) {
-							VideoAPI videoAPI = videosFromAPI.get(i);
-							
-							if (videoServ.readCompletedByVideoId(videoAPI.getId()) != null)
-								continue;
-							
-							VideoDTO completed = new VideoDTO();
-							completed.setMemberName(member.getMemberName());
-							completed.setChannelId(member.getChannelId());
-							completed.setVideoId(videoAPI.getId());
-							completed.setScheduledStartTime(videoAPI.getStart_scheduled());
-							completed.setActualStartTime(videoAPI.getStart_actual());
-							completed.setActualEndTime(videoAPI.getEnd_actual());
-							
-							if (completed.getActualStartTime() == null)
-								continue;
-							
-							videoServ.createCompleted(completed);
-							recorded++;
-						}
-						long endTime = Calendar.getInstance().getTimeInMillis();
-						System.out.println("총 " + recorded + "개가 입력되었습니다. (동작 시간 : " + (endTime-startTime) + ")");
-						offset += 50;
-					}
-					
-					Thread.sleep(1000);
-				}
-			} catch (IOException | InterruptedException e) {
-				e.printStackTrace();
-			}
-
-		}
-	}
+//	@RequestMapping("/collect")
+//	public String collect() {
+//		checkChannel();
+//		return "/collect";
+//	}
+//
+//	@Autowired
+//	private VideoService videoServ;
+//
+//	ObjectMapper objectMapper = new ObjectMapper();
+//	DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+//
+//	@Async("executor")
+//	public void checkChannel() {
+//		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//		List<MemberDTO> members = videoServ.readAllMember();
+//
+//		for (MemberDTO member : members) {
+//			System.out.println(member.getMemberName());
+//			int offset = 0;
+//			try {
+//				while (true) {
+//					long startTime = Calendar.getInstance().getTimeInMillis();
+//					System.out.print("\toffset : " + offset + " => ");
+//					
+//					String videoURL = "https://holodex.net/api/v2/videos?channel_id=" + member.getChannelId() + "&status=past&type=stream&include=live_info&order=asc&limit=50&offset=" + offset;
+//					
+//					HttpRequest request = HttpRequest.newBuilder().uri(URI.create(videoURL))
+//							.header("Content-Type", "application/json").header("X-APIKEY", "27e71dd9-f108-4896-875e-5a91207a63fe")
+//							.method("GET", HttpRequest.BodyPublishers.noBody()).build();
+//					HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+//					List<VideoAPI> videosFromAPI = objectMapper.readValue(response.body(), objectMapper.getTypeFactory().constructCollectionType(List.class, VideoAPI.class));
+//					if (videosFromAPI.size() == 0) {
+//						System.out.println("더 이상 영상이 없습니다.");
+//						break;
+//					}
+//					else {
+//						int recorded = 0;
+//						for (int i = 0; i < videosFromAPI.size(); i++) {
+//							VideoAPI videoAPI = videosFromAPI.get(i);
+//							
+//							if (videoServ.readCompletedByVideoId(videoAPI.getId()) != null)
+//								continue;
+//							
+//							VideoDTO completed = new VideoDTO();
+//							completed.setMemberName(member.getMemberName());
+//							completed.setChannelId(member.getChannelId());
+//							completed.setVideoId(videoAPI.getId());
+//							completed.setScheduledStartTime(videoAPI.getStart_scheduled());
+//							completed.setActualStartTime(videoAPI.getStart_actual());
+//							completed.setActualEndTime(videoAPI.getEnd_actual());
+//							
+//							if (completed.getActualStartTime() == null)
+//								continue;
+//							
+//							videoServ.createCompleted(completed);
+//							recorded++;
+//						}
+//						long endTime = Calendar.getInstance().getTimeInMillis();
+//						System.out.println("총 " + recorded + "개가 입력되었습니다. (동작 시간 : " + (endTime-startTime) + ")");
+//						offset += 50;
+//					}
+//					
+//					Thread.sleep(1000);
+//				}
+//			} catch (IOException | InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//
+//		}
+//	}
 
 }
